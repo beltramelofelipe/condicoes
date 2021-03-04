f580f04253fac3be8e8c5da30260e3f0a5c2e095
Felipe Beltramelo <beltramelofelipe@gmail.com>
2021-03-04 10:29:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+km = int(input('Qual a Velocidade em KM: '))
+multa = km * 7
+if km > 80:
+   print('Você foi multado em {: .2f}'.format(multa))
+else:
+   print('Velocidade normal!')
+
+
+# num = int(input('Digite um número entre 0 e 5: '))
+
+# if num == 5:
+#    print('Você acertou!!!!')
+# else:
+#    print('Você errou!!!!')
+
+# n1 = float(input('Digite a primeira nota: '))
+# n2 = float(input('Digite a segunda nota: '))
+# m = (n1 + n2) / 2
+# print('A sua média foi {: .1f}'.format(m))
+# if m >= 6:
+#   print('Sua média foi boa, PARABENS!')
+# else:
+#   print('Sua média foi ruim, ESTUDE')
+
